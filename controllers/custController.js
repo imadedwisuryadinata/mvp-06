76c681200c47e6cecae99b67db3388b0fe87fe7c
imadedwisuryadinata <dwi.suryadinata@gmail.com>
2020-11-24 10:47:24 +0800

    penambahan fitur pemberian rating oleh customer

--- controllers/custController.js
+++ controllers/custController.js
@@ -270,4 +270,56 @@ customerRouter.get('/feedback', async (req, res) => {
     })
 })
 
+//melihat daftar ticket yang perlu dirating
+customerRouter.get('/rating', async (req, res) => {
+    //header apabila akan melakukan akses
+    var authHeader = req.headers.authorization;
+    if (!authHeader) 
+        return res.status(401).send({ auth: false, message: 'No token provided.' });
+    const token = authHeader.split(' ')[1];
+    
+    //verifikasi jwt
+    jwt.verify(token, Conf.secret, async(err,user) => {
+      if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+        const id = user.id
+        const ticket = await Ticket.find({"status": 5, "customer_id" : id});
+        res.json(ticket)
+
+    })
+})
+
+// memberikan rating
+customerRouter.patch('/rating/:id', async (req, res) => {    
+    //header apabila akan melakukan akses
+    var authHeader = req.headers.authorization;
+    if (!authHeader) 
+        return res.status(401).send({ auth: false, message: 'No token provided.' });
+    const token = authHeader.split(' ')[1];
+    
+    //verifikasi jwt
+    jwt.verify(token, Conf.secret, async(err, user) => {
+      if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+      const csId = user.id 
+      try {
+        
+        const {rate_from_cust} = req.body;
+        const ticket = await Ticket.findById(req.params.id);
+        if(ticket){
+            ticket.rate_from_cust = rate_from_cust
+            const updatedTicket = await ticket.save();
+            res.json(updatedTicket);
+        } else {
+            res.status(404).json({
+                message: 'ticket not found'
+            })
+        }
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({"status": "user not found"});
+    }
+
+    })
+})
+
 export default customerRouter
